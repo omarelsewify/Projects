e76d89c9dd0c20596db3e84a96f2112e0302fb47
omarelsewify <79166701+omarelsewify@users.noreply.github.com>
2022-05-30 11:58:52 -0700

    Final version with tuned hyperparameters

--- Engineering_Optimization/Controller Optimisation.py
+++ Engineering_Optimization/Controller Optimisation.py
@@ -4,8 +4,11 @@ import numpy as np
 import pandas as pd
 import openpyxl 
 from openpyxl import load_workbook
+from random import choices
 
 import matplotlib.pyplot as plt
+import matplotlib
+
 from numpy.random import permutation as randperm
 from scipy.interpolate import Rbf # Import Radial Basis fitting package from scipy
 from pytictoc import TicToc
@@ -13,7 +16,7 @@ from pytictoc import TicToc
 t = TicToc()
 
 plotting = 1
-savedata = 0
+savedata = 1
 
 class TrainTest:
     def __init__(self):
@@ -49,11 +52,35 @@ def cross_validation_estimate(X, y, sets, fit, metric):
 
     return sum(error_estimate)/len(tt)
 
-# Cross Validation Metric
+# Expected Square Error
 def metric(f, X, y):
     output = sum([(f(X["K_la"].iloc[i], X["x_la"].iloc[i], X["K_long"].iloc[i]) - y.iloc[i])**2 for i in range(len(X)-1)])/len(X)
     return output
 
+##################################################### Bootstrap
+
+# Bootstrap sets generation
+def bootstrap_sets(m,b):
+    
+    bs_set = TrainTest()
+    
+    for i in range(b):
+        train = choices(range(m), k=m)
+        test = list(range(m))
+        
+        bs_set.Train.append(train)
+        bs_set.Test.append(test)
+    
+    return bs_set 
+
+def bootstrap_estimate(X, y, sets, fit, metric):
+    
+    tt = [[sets.Train[i], sets.Test[i]] for i in range(len(sets.Test))] 
+    error_estimate = [train_and_validate(X,y,tt[i],fit,metric) for i in range(len(tt))]
+
+    return sum(error_estimate)/len(tt)
+
+    
 ##################################################### Import Excel data
 
 # Read in dataset from Excel
@@ -70,7 +97,6 @@ workbook = pd.read_excel("Sim3_Results.xlsx",
 sim3_data = workbook[workbook['Simulation Mode']==3]    # Actuator dynamics,noisy measurements, and hold period
 
 # sim_data_all = [sim0_data,sim1_data,sim2_data,sim3_data]
-sim_data_all = [sim3_data]
 
 ##################################################### Filtering data set to most elite solutions
 
@@ -81,7 +107,9 @@ sim_data_all = [sim3_data]
 
 # ranking = np.argsort(sample_scores)
 
-sim3_data = sim3_data.iloc[0:31,:] # consider only the first 31 rows, the rest are results of simulation
+# sim3_data = sim3_data.iloc[0:31,:] # consider only the first 31 rows, the rest are results of simulation
+
+sim_data_all = [sim3_data]
 
 ##################################################### Optimising hyperparameter of surrogate model
 
@@ -106,16 +134,22 @@ for sim_data in sim_data_all:
     max_ax = sim_data["Max Longitudinal Acceleration"]
 
     # Split the dataset into k folds
-    sets = TrainTest()
-    sets.reset()
-    sets = k_fold_cross_validation_sets(len(X),4)   # k = 4
+    sets_cv = TrainTest()
+    sets_cv.reset()
+    sets_cv = k_fold_cross_validation_sets(len(sim_data),4)   # k = 4
 
+    # Split the dataset into bootstrap sets
+    sets_bs = TrainTest()
+    sets_bs.reset()
+    m, b = len(sim_data), 10
+    sets_bs = bootstrap_sets(m,b)
+    
     est_lat_errors = []
     est_speed_errors = []
     est_max_ay = []
     est_max_ax = []
 
-    smooth_params = 10**(np.linspace(start = -10, stop = -2, num=101))
+    smooth_params = 10**(np.linspace(start = -10, stop = -2, num=51))
 
     for smooth_p in smooth_params:
         # Fit Radial Basis Functions to Simulation Mode 0
@@ -125,10 +159,15 @@ for sim_data in sim_data_all:
         rbfi_max_ay = Rbf(k_la, x_la, k_long, max_ay, function = "linear", smooth = smooth_p)
         rbfi_max_ax = Rbf(k_la, x_la, k_long, max_ax, function = "linear", smooth = smooth_p)
         
-        gen_error_lat_model = cross_validation_estimate(X, max_lat_error, sets, rbfi_lat_error, metric)
-        gen_error_speed_model = cross_validation_estimate(X, max_speed_error, sets, rbfi_speed_error, metric)
-        gen_max_ay_model = cross_validation_estimate(X, max_ay, sets, rbfi_max_ay, metric)
-        gen_max_ax_model = cross_validation_estimate(X, max_ax, sets, rbfi_max_ax, metric)
+        # gen_error_lat_model = cross_validation_estimate(X, max_lat_error, sets_cv, rbfi_lat_error, metric)
+        # gen_error_speed_model = cross_validation_estimate(X, max_speed_error, sets_cv, rbfi_speed_error, metric)
+        # gen_max_ay_model = cross_validation_estimate(X, max_ay, sets_cv, rbfi_max_ay, metric)
+        # gen_max_ax_model = cross_validation_estimate(X, max_ax, sets_cv, rbfi_max_ax, metric)
+        
+        gen_error_lat_model = bootstrap_estimate(X, max_lat_error, sets_bs, rbfi_lat_error, metric)
+        gen_error_speed_model = bootstrap_estimate(X, max_speed_error, sets_bs, rbfi_speed_error, metric)
+        gen_max_ay_model = bootstrap_estimate(X, max_ay, sets_bs, rbfi_max_ay, metric)
+        gen_max_ax_model = bootstrap_estimate(X, max_ax, sets_bs, rbfi_max_ax, metric)
         
         est_lat_errors.append(gen_error_lat_model)
         est_speed_errors.append(gen_error_speed_model)
@@ -158,7 +197,7 @@ for sim_data in sim_data_all:
     print('')
     
     # Plotting of Estimated errors vs Smoothing Parameters
-    if plotting:
+    if 0:
         plt.title("Simulator Mode %d" % sim_number)
         plt.plot(smooth_params, est_lat_errors, 'g')
         plt.plot(smooth_params, est_speed_errors, 'b')
@@ -172,14 +211,26 @@ for sim_data in sim_data_all:
         plt.plot(optimal_smooth_param_speed,np.amin(est_speed_errors[0:]),'bo')
         plt.plot(optimal_smooth_param_lat,np.amin(est_max_ay[0:]),'ro')
         plt.plot(optimal_smooth_param_speed,np.amin(est_max_ax[0:]),'mo')
-        plt.show()
-    
+        
+        # plt.savefig('smooth_param.pgf')
+
+        # plt.show()
+
+##################################################### Obj Function
+
+def obj_function(x,f,c,rho):
+    weights = [10, 10, 3, 3]
+    weighted_c = sum([rho * a * b for a, b in zip(weights, c(x))])
+    return  sum(f(x)) + weighted_c     # Quadratic Penalty
+      
 ##################################################### Cross Entropy with Interior Point Method
 
 def cross_entropy_method_interior_point(f, x0, m, m_elite, rho, gamma, tol):
      
-    x_history = [x0]
-
+    x_history = []
+    y_history = []
+    obj_history = []
+    
     # Initialising P
     xdata = np.random.rand(m_elite,len(x0))  
     mean = x0
@@ -235,9 +286,11 @@ def cross_entropy_method_interior_point(f, x0, m, m_elite, rho, gamma, tol):
             IPM_sum_temp = sum([ci**2 for ci in constraint])
             IPM_sum.append(IPM_sum_temp)
         
-        min_function = [f(samples[i]) + rho*IPM_sum[i] for i in range(m)]
-   
-        IPM_order = np.argsort([(sum(x))for x in min_function])    
+        # min_function = [f(samples[i]) + rho*IPM_sum[i] for i in range(m)]
+        
+        min_function = [obj_function(samples[i],f,c,rho) for i in range(m)]
+        
+        IPM_order = np.argsort(min_function)    
         IPM_elite_samples = samples[IPM_order[0:m_elite]]
         
         mean = np.array([sum(x)/len(x) for x in zip(*IPM_elite_samples)])
@@ -245,10 +298,14 @@ def cross_entropy_method_interior_point(f, x0, m, m_elite, rho, gamma, tol):
         delta = np.linalg.norm(mean - mean_old)
         mean_old = mean
         x_history.append(mean)
+        y_history.append(f(mean))
+        
+        IPM_sum_temp = sum([ci**2 for ci in c(mean)])
+        obj_history.append(sum(f(mean)) + IPM_sum_temp)
         
         rho *= gamma
 
-    return x_history
+    return x_history, y_history, obj_history
 
 ##################################################### Hooke-Jeeves
 def basis(i,n):
@@ -258,6 +315,8 @@ def hooke_jeeves(f, x0, alpha, eps, gamma, rho):
     
     x_history = []
     y_history = []
+    obj_history = []
+    
     n = len(x0)
     x_best = x0
     
@@ -276,11 +335,6 @@ def hooke_jeeves(f, x0, alpha, eps, gamma, rho):
         
         return [mle_c, mse_c, may_c, max_c, K_la_c, x_la_c, K_long_c]
     
-    def obj_function(x,f,c,rho):
-        weights = [10, 10, 1, 1]
-        weighted_c = sum([rho * a * b for a, b in zip(weights, c(x))])
-        return  sum(f(x)) + weighted_c     # Quadratic Penalty
-    
     while alpha > eps:
         
         improved = False
@@ -290,20 +344,22 @@ def hooke_jeeves(f, x0, alpha, eps, gamma, rho):
         for i in range(n): 
             for sgn in (-1,1):
                 vector = [sgn*alpha*idx for idx in basis(i,n)] 
-                x_temp = [x[i]*(1 + vector[i]) for i in range(n)]
+                x_temp = [x[i]*(1+vector[i]) for i in range(n)]
                 #eps = abs(min([x[i]*(vector[i]) for i in range(n)]))
                 obj_temp = obj_function(x_temp,f,c,rho)
                 
                 if obj_temp < obj_best:
                     x_best = x_temp
-                    x_history.append(x_best)
-                    y_history.append(obj_temp)
                     improved = True
+                
+        x_history.append(x_best)
+        obj_history.append(obj_best)
+        y_history.append(f(x_best))   
                 
         if improved == False:
             alpha *= gamma
     
-    return x_history, y_history
+    return x_history, y_history , obj_history
 
 ##################################################### Design Point optimisation of the Surrogate Models
 
@@ -343,23 +399,31 @@ for sim_mode in estimator_functions:
           np.random.uniform(min(x_la),max(x_la)),
           np.random.uniform(min(k_long),max(k_long))]
     
-    # t.tic()
-    # x_history = cross_entropy_method_interior_point(f, x0, m = 50, m_elite = 10, rho = 25, gamma = 1.2, tol = 0.01)
-    # t.toc()
-    
     t.tic()
-    x_history, y_history = hooke_jeeves(f, x0, alpha = 0.1, eps = 0.001 , gamma = 0.9, rho = 10)
+    x_history_ce, y_history_ce, obj_history_ce = cross_entropy_method_interior_point(f, x0, m = 25, m_elite = 5, rho = 2, gamma = 1.5, tol = 0.01)
     t.toc()
     
-    x_best = x_history[-1]
-    print("Optimal PID Gains for Simulation Mode %d:" %sim_number)
-    print(x_best)
+    t.tic()
+    x_history_hj, y_history_hj, obj_history_hj = hooke_jeeves(f, x0, alpha = 0.005, eps = 0.00001 , gamma = 0.5, rho = 2)
+    t.toc()
+    
+    x_best_ce = list(x_history_ce[-1])
+    print("Optimal PID Gains from Cross-Entropy: ")
+    print(x_best_ce)
     print("")
-
+    
+    x_best_hj = x_history_hj[-1]
+    print("Optimal PID Gains from Hooke-Jeeves: ")
+    print(x_best_hj)
+    print("")
+    
     # Save data to excel sheet
     if savedata:
-        data2save = [3, x_best[0], x_best[1], x_best[2]]
+        data2save = [3, x_best_ce[0], x_best_ce[1], x_best_ce[2]]
+        data2save2 = [3, x_best_hj[0], x_best_hj[1], x_best_hj[2]]
+        
         df = pd.DataFrame([data2save])
+        df2 = pd.DataFrame([data2save2])
         
         path = "Sim3_Results.xlsx"
         
@@ -370,21 +434,47 @@ for sim_mode in estimator_functions:
 
         df.style.set_properties(**{'background-color': 'yellow','color': 'black'})
         df.to_excel(writer,sheet_name='Sheet1', startrow=writer.sheets['Sheet1'].max_row, index = False,header= False)
-
+        df2.style.set_properties(**{'background-color': 'yellow','color': 'black'})
+        df2.to_excel(writer,sheet_name='Sheet1', startrow=writer.sheets['Sheet1'].max_row, index = False,header= False)
+        
         writer.save()
     
-    plt.plot(range(len(x_history)),[x[0] for x in x_history])
+    fig1 = plt.figure()
+    plt.plot(range(len(x_history_ce)),[x[0] for x in x_history_ce])
+    plt.plot(range(len(x_history_hj)),[x[0] for x in x_history_hj])
+    plt.legend(["Cross-Entropy","Hooke-Jeeves"])
     plt.title('K_la evolution')
     plt.show()
     
-    plt.plot(range(len(x_history)),[x[1] for x in x_history])
+    fig2 = plt.figure()
+    plt.plot(range(len(x_history_ce)),[x[1] for x in x_history_ce])
+    plt.plot(range(len(x_history_hj)),[x[1] for x in x_history_hj])
+    plt.legend(["Cross-Entropy","Hooke-Jeeves"])
     plt.title('x_la evolution')
     plt.show()
     
-    plt.plot(range(len(x_history)),[x[2] for x in x_history])
+    fig3 = plt.figure()
+    plt.plot(range(len(x_history_ce)),[x[2] for x in x_history_ce])
+    plt.plot(range(len(x_history_hj)),[x[2] for x in x_history_hj])
+    plt.legend(["Cross-Entropy","Hooke-Jeeves"])
     plt.title('K_long evolution')
     plt.show()
     
-    plt.plot(range(len(y_history)),y_history)
+    fig4 = plt.figure()
+    plt.plot(range(len(obj_history_ce)),obj_history_ce)
+    plt.plot(range(len(obj_history_hj)),obj_history_hj)
+    plt.legend(["Cross-Entropy","Hooke-Jeeves"])
     plt.title('Objective function')
     plt.show()
+    
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
+fig1.savefig('k_la.pgf')
+fig2.savefig('x_la.pgf')
+fig3.savefig('k_long.pgf')
+fig4.savefig('objective_function.pgf')
